6ed7c0d6701fa0cfd54576c2475dfa166936b83d
Viper76Man <nick@bind-tech.com>
2026-03-17 15:46:50 -0400

    Added mecanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/BasicDriveTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.coach;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.coach.subsystems.MecanumDriveSub;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+
+@TeleOp(name = "Basic Drive Test", group = "Coach")
+public class BasicDriveTest extends NextFTCOpMode {
+
+    public BasicDriveTest(){
+        addComponents(
+                new SubsystemComponent(MecanumDriveSub.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+
+    @Override
+    public void onInit(){
+        //Init goes here
+    }
+    public void onStartButtonPressed() {
+        telemetry.addLine("Running");
+
+        Command driveControlled = new MecanumDriverControlled(
+                MecanumDriveSub.INSTANCE.frontLeft,
+                MecanumDriveSub.INSTANCE.frontRight,
+                MecanumDriveSub.INSTANCE.backLeft,
+                MecanumDriveSub.INSTANCE.backRight,
+                Gamepads.gamepad1().leftStickY().negate(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX()
+        );
+        driveControlled.schedule();
+    }
+
+    @Override
+    public void onUpdate() {
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/subsystems/MecanumDriveSub.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.coach.subsystems;
+
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class MecanumDriveSub implements Subsystem {
+    public final MotorEx frontLeft = new MotorEx("fl").brakeMode().reversed();
+    public final MotorEx frontRight = new MotorEx("fr").brakeMode();
+    public final MotorEx backLeft = new MotorEx("bl").brakeMode().reversed();
+    public final MotorEx backRight = new MotorEx("br").brakeMode();
+
+    public static final MecanumDriveSub INSTANCE = new MecanumDriveSub();
+}
