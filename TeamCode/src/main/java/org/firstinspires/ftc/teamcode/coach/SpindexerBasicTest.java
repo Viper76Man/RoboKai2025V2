c0c8290c4ad6abd9ed04cbfb911c36c8dc58b5ff
Viper76Man <nick@bind-tech.com>
2026-03-18 15:26:42 -0400

    Updated spindexer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/SpindexerBasicTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/SpindexerBasicTest.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.coach.subsystems.SpindexerSub;
 
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 
@@ -28,11 +29,19 @@ public class SpindexerBasicTest extends NextFTCOpMode {
     @Override
     public void onStartButtonPressed(){
         telemetry.addLine("Running");
+
+        Gamepads.gamepad1().dpadDown()
+                .whenBecomesTrue(SpindexerSub.INSTANCE.toFirstPos());
+
+        Gamepads.gamepad1().dpadLeft()
+                .whenBecomesTrue(SpindexerSub.INSTANCE.toSecondPOS());
     }
+
 
     @Override
     public void onUpdate() {
         telemetry.addData("Spindexer Position", SpindexerSub.INSTANCE.getSpindexerPosition());
+        telemetry.addData("Last Command", SpindexerSub.INSTANCE.lastCommand);
         telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/subsystems/SpindexerSub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/subsystems/SpindexerSub.java
@@ -1,15 +1,62 @@
 package org.firstinspires.ftc.teamcode.coach.subsystems;
 
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.RunToPosition;
 import dev.nextftc.hardware.impl.MotorEx;
 
 public class SpindexerSub implements Subsystem {
     public static final SpindexerSub INSTANCE = new SpindexerSub();
     private SpindexerSub(){}
 
+    private static double firstPos = 0; //0 degrees
+    private static double secondPos = 1333.33; //120 degrees
+    private static double thirdPos = 2666.66; //240 degrees
+    private static double shoot = 6666.66; //600 degrees
+    public String lastCommand = "None";
     private final MotorEx motor = new MotorEx("spindexer");
+
+    private final ControlSystem controlSystem = ControlSystem.builder()
+            .posPid(0.005, 0, 0)
+            .build();
 
     public double getSpindexerPosition(){
         return motor.getCurrentPosition();
     }
+
+
+
+    //public Command toFirstPos = new RunToPosition(controlSystem, firstPos).requires(this);
+    //public Command toSecondPOS = new RunToPosition(controlSystem, secondPos).requires(this);
+    public Command toThirdPos = new RunToPosition(controlSystem, thirdPos).requires(this);
+    public Command toShoot = new RunToPosition(controlSystem, shoot).requires(this);
+
+    public Command toFirstPos(){
+        lastCommand = "0 Degrees";
+        new RunToPosition(controlSystem, firstPos).requires(this).schedule();
+        return null;
+    }
+
+    public Command toSecondPOS(){
+        lastCommand = "120 Degrees";
+        new RunToPosition(controlSystem, secondPos).requires(this).schedule();
+        return null;
+    }
+
+    @Override
+    public void initialize() {
+        //reset encoder
+        motor.zero();
+    }
+
+    @Override
+    public void periodic() {
+        motor.setPower(controlSystem.calculate(motor.getState()));
+    }
+
+
+
+
+
 }
