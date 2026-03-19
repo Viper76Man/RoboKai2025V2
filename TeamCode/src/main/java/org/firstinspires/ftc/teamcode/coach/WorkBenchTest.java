2786aeb8679adda082e22ebc10711c83298a0734
Viper76Man <nick@bind-tech.com>
2026-03-19 11:22:22 -0400

    Added comments to workbench exercise

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/WorkBenchTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/WorkBenchTest.java
@@ -11,10 +11,13 @@ import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
-
+//Add TeleOp with name and group
 @TeleOp(name = "Work Bench Test", group = "Coach")
+//Update class to extends NextFTCOpMode
 public class WorkBenchTest extends NextFTCOpMode {
 
+    //This is how we add components to allow us to access public statements in our subsystem
+    //and also add bulk read and PS5 bindings
     public WorkBenchTest(){
         addComponents(
                 new SubsystemComponent(WorkbenchSub.INSTANCE),
@@ -23,15 +26,19 @@ public class WorkBenchTest extends NextFTCOpMode {
         );
     }
 
+    //Init stuff goes here
     @Override
     public void onInit(){
-        //Init stuff goes here
+
     }
 
+    //Start button pressed stuff goes here
     @Override
     public void onStartButtonPressed(){
+        //Simple telemetry statement to out put to hub
         telemetry.addLine("Running");
 
+        //Special mecanum drive control command
         Command driveControlled = new MecanumDriverControlled(
                 WorkbenchSub.INSTANCE.frontLeft,
                 WorkbenchSub.INSTANCE.frontRight,
@@ -41,11 +48,14 @@ public class WorkBenchTest extends NextFTCOpMode {
                 Gamepads.gamepad1().leftStickX(),
                 Gamepads.gamepad1().rightStickX()
         );
+        //Need schedule to make it run
         driveControlled.schedule();
     }
 
+    //Code to run on every loop
     @Override
     public void onUpdate(){
+        //Use this to update hub on every loop
         telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/subsystems/WorkbenchSub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coach/subsystems/WorkbenchSub.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode.coach.subsystems;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.impl.MotorEx;
 
+//Update class to use implements Subsystem
 public class WorkbenchSub implements Subsystem {
+    //Always use these 2 statements to create a singleton
     public static final WorkbenchSub INSTANCE = new WorkbenchSub();
     private WorkbenchSub(){}
 
+    //Example of MotorEx instead of DcMotorEx
     public final MotorEx frontLeft = new MotorEx("fl").brakeMode().reversed();
     public final MotorEx frontRight = new MotorEx("fr").brakeMode();
     public final MotorEx backLeft = new MotorEx("bl").brakeMode().reversed();
