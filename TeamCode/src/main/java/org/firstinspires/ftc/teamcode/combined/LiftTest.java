1e7f26e9b5a367b61e0ab39b58e6557a1d9c2e1c
Viper76Man <nick@bind-tech.com>
2026-05-27 14:12:56 -0400

    Added small fixes to lift

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/combined/LiftTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.combined;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.combined.subsystems.LiftSub2;
+
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@TeleOp(name = "Lift Test", group = "Coach")
+public class LiftTest extends NextFTCOpMode {
+
+    public LiftTest(){
+        addComponents(
+                new SubsystemComponent(LiftSub2.Instance),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        Gamepads.gamepad1().dpadUp()
+                .whenBecomesTrue(LiftSub2.Instance.goToPosition());
+    }
+}
